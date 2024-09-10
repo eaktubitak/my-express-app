dafe52c6f3c244206293b961ae69a05d85e81332
ensar.kurubacak <eak.tubitak@gmail.com>
2024-09-10 13:09:42 +0000

    #8: module.exports = app; line added to index.js file.

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-// index.js
 const express = require('express');
 const app = express();
+
+// Route tanımı
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-app.listen(5000, () => console.log('Server is up and running'));
+  res.send('Hello World!');
+});
+
+// Sunucu başlatma
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Server is up and running on port ${PORT}`));
+
+// app nesnesini export et
+module.exports = app;
--- test/index.test.js
+++ test/index.test.js
@@ -1,13 +1,17 @@
 const request = require('supertest');
-const express = require('express');
-const assert = require('assert');
 const app = require('../index'); // Ana uygulama dosyasını CommonJS ile import edin
 
 describe('GET /', function() {
-  it('should return Hello World!', function(done) {
-    request(app)
+  it('should return Hello World!', async function() {
+    // Supertest ile async/await kullanarak test yapma
+    const response = await request(app)
       .get('/')
       .expect('Content-Type', /text/)
-      .expect(200, 'Hello World!', done);
+      .expect(200);
+    
+    // Yanıtın içeriğini kontrol etme
+    if (response.text !== 'Hello World!') {
+      throw new Error(`Beklenen 'Hello World!' değeri yerine ${response.text} döndürüldü.`);
+    }
   });
 });
