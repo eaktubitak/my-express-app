308ec5716ae54fa430c42a4674428d6063d5298a
ensar.kurubacak <eak.tubitak@gmail.com>
2024-09-10 12:47:18 +0000

    #6: Test stage added, new test cases created under /test folder. (index.test.js file changed)

--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,7 @@
 const request = require('supertest');
 const express = require('express');
-const app = require('../index'); // Uygulamanızın ana dosyasını buraya import edin
 const { expect } = require('chai');
+const app = require('../index'); // Ana uygulama dosyasını CommonJS ile import edin
 
 describe('GET /', function() {
   it('should return Hello World!', function(done) {
@@ -9,13 +9,5 @@ describe('GET /', function() {
       .get('/')
       .expect('Content-Type', /text/)
       .expect(200, 'Hello World!', done);
-  });
-});
-
-describe('Server', function() {
-  it('should be listening on port 5000', function(done) {
-    // Port kontrolü yapmak genellikle zordur ve genellikle sistem üzerinde yapılır
-    // Bu test, sadece sunucunun başlatıldığını doğrulamak için bir yer tutucudur
-    done();
   });
 });
